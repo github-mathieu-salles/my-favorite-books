fc87fce4074f42cc7ef8cc600863bbfdefc58524
mathieuSal <mathieu.salles@swile.co>
2024-11-24 11:41:46 +0100

    feat(my-favorite-books): add caption to table

--- src/_designSystem/table/table.jsx
+++ src/_designSystem/table/table.jsx
@@ -2,26 +2,27 @@ import {LoadingIndicator} from "../../components/loadingIndicator/loadingIndicat
 
 import './table.scss'
 
-export const Table = ({ meta, data, isLoading}) => {
+export const Table = ({ meta: { caption, columns }, data, isLoading }) => {
   if (data.length === 0) return null
   return (
     <table className="Table">
+      <caption>{caption}</caption>
       <thead>
       <tr>
-        {meta.map(({header}) => (
+        {columns.map(({header}) => (
           <th key={header}>{header}</th>
         ))}
       </tr>
       </thead>
       {!isLoading
         ? <tbody>
-          {data.map((row) => (
-            <tr key={row.title}>
-              {meta.map(({key, transformer}) => (
-                <td key={row[key]}>{transformer ? transformer(row[key]) : row[key]}</td>
-              ))}
-            </tr>
-          ))}
+        {data.map((row) => (
+          <tr key={row.title}>
+            {columns.map(({key, transformer}) => (
+              <td key={row[key]}>{transformer ? transformer(row[key]) : row[key]}</td>
+            ))}
+          </tr>
+        ))}
         </tbody>
         : <div className="Table-body--loading">
           <LoadingIndicator isLoading={isLoading}/>
--- src/components/favoriteBooksTable/favoriteBookTable.jsx
+++ src/components/favoriteBooksTable/favoriteBookTable.jsx
@@ -3,25 +3,28 @@ import {capitalize} from "../../helpers/string.jsx";
 
 export const FavoriteBookTable = ({ books, isLoading }) => {
 
-  const meta = [
-    {
-      header: 'title',
-      key: 'title',
-    },
-    {
-      header: 'author',
-      key: 'author',
-    },
-    {
-      header: 'year',
-      key: 'year',
-    },
-    {
-      header: 'genre',
-      key: 'genre',
-      transformer: (genre) => genre.map(capitalize).join(', '),
-    },
-  ]
+  const meta = {
+    caption: 'Top 10 Favorite Books',
+    columns: [
+      {
+        header: 'title',
+        key: 'title',
+      },
+      {
+        header: 'author',
+        key: 'author',
+      },
+      {
+        header: 'year',
+        key: 'year',
+      },
+      {
+        header: 'genre',
+        key: 'genre',
+        transformer: (genre) => genre.map(capitalize).join(', '),
+      },
+    ]
+  }
   return (
     <Table meta={meta} data={books} isLoading={isLoading} />
   )
